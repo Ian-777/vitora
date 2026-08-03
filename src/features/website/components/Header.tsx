a0de4d8969ec04e2f589cf45e73c750b164e95af
Ian Fernando <fer.nando.996v@gmail.com>
2026-08-02 19:39:34 -0500

    responsive

--- src/features/website/components/Header.tsx
+++ src/features/website/components/Header.tsx
@@ -1,63 +1,46 @@
-import {
-  Avatar,
-  IconButton,
-  Card,
-  Typography,
-} from "@/shared/ui";
+import { Avatar, Card, IconButton, Typography } from "@/shared/ui";
 
-import {
-  Globe,
-  MapPin,
-} from "lucide-react";
+import { Globe, MapPin, Download } from "lucide-react";
+
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 import { profile } from "../profile.mock";
 
 export function Header() {
   return (
-    <Card className="flex flex-col items-center text-center">
-      <Avatar />
+    <Card>
+      <div className="flex flex-col items-center gap-8 text-center md:flex-row md:items-center md:gap-14 md:px-6 text-center">
+        <div className="flex w-52 justify-center">
+          <Avatar />
+        </div>
 
-      <Typography
-        variant="h1"
-        className="mt-6"
-      >
-        {profile.name}
-      </Typography>
+        <div className="flex flex-1 flex-col items-center">
+          <Typography variant="h1" className="mt-2 md:mt-0">
+            {profile.name}
+          </Typography>
 
-      <Typography
-        variant="muted"
-        className="mt-2"
-      >
-        {profile.title}
-      </Typography>
+          <Typography variant="muted" className="mt-2">
+            {profile.title}
+          </Typography>
 
-      <div className="mt-4 flex items-center gap-2 text-[var(--muted-foreground)]">
-        <MapPin size={18} />
+          <div className="mt-4 flex items-center justify-center gap-2 justify-center text-[var(--muted-foreground)]">
+            <MapPin size={18} />
 
-        <Typography variant="body">
-          {profile.location}
-        </Typography>
-      </div>
+            <Typography variant="body">{profile.location}</Typography>
+          </div>
 
-      <div className="mt-8 flex flex-wrap justify-center gap-3">
-        
-        <IconButton 
-         /* icon={<GitHub size={18} />}
-        >
-          GitHub
-        </IconButton>
+          <div className="mt-6 flex flex-wrap justify-center gap-3 md:justify-start">
+            <IconButton icon={<FaGithub size={18} />}>GitHub</IconButton>
 
-        <IconButton
-          icon={<Linkedin size={18} />}
-        >
-          LinkedIn
-        </IconButton> 
+            <IconButton icon={<FaLinkedin size={18} />}>LinkedIn</IconButton>
 
-        <IconButton */
-          icon={<Globe size={18} />}
-        >
-          Website
-        </IconButton>
+            <IconButton icon={<Globe size={18} />}>Website</IconButton>
+          </div>
+
+          <div className="mt-6 flex justify-center md:justify-start">
+            <IconButton icon={<Download size={18} />}>Download CV</IconButton>
+          </div>
+        </div>
       </div>
     </Card>
   );
