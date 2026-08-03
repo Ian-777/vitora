2b4904a10a962c038f388c0e10d8170dce8a22c2
Ian Fernando <fer.nando.996v@gmail.com>
2026-08-02 22:30:49 -0500

    Vitora 1.1

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+
+import { TooltipProvider } from "@/components/ui/tooltip";
+
 import App from "./App.tsx";
+
+import "./index.css";
 import "./styles/index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <TooltipProvider>
+      <App />
+    </TooltipProvider>
   </StrictMode>,
 );
